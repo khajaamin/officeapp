03e243f26f6f891118063345c11e5c53945d7568
Khajaamin Attar <khajaamin@gmail.com>
2016-05-24 16:32:50 +0530

    ionic 2 started for office

---
+++ app/app.js
@@ -0,0 +1,51 @@
+import {ViewChild} from '@angular/core';
+import {App, Platform, MenuController} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HelloIonicPage} from './pages/hello-ionic/hello-ionic';
+import {ListPage} from './pages/list/list';
+import {AddNotePage} from './pages/add-note/add-note';
+
+
+@App({
+  templateUrl: 'build/app.html',
+  config: {}, // http://ionicframework.com/docs/v2/api/config/Config/
+  queries: {
+    nav: new ViewChild('content')
+  }
+})
+class MyApp {
+  static get parameters() {
+    return [[Platform], [MenuController]];
+  }
+
+  constructor(platform, menu) {
+    this.platform = platform;
+    this.menu = menu;
+    this.initializeApp();
+
+    // set our app's pages
+    this.pages = [
+      { title: 'My Office Diary', component: HelloIonicPage },
+      { title: 'My Note List', component: ListPage },
+      { title: 'Add Note', component: AddNotePage }
+    ];
+
+    // make HelloIonicPage the root (or first) page
+    this.rootPage = HelloIonicPage;
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+
+  openPage(page) {
+    // close the menu when clicking a link from the menu
+    this.menu.close();
+    // navigate to the new page if it is not the current page
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ app/pages/list/list.js
@@ -0,0 +1,39 @@
+import {Page, NavController, NavParams} from 'ionic-angular';
+import {ItemDetailsPage} from '../item-details/item-details';
+import {AddNotePage} from './pages/add-note/add-note';
+
+
+@Page({
+  templateUrl: 'build/pages/list/list.html'
+})
+export class ListPage {
+  static get parameters() {
+    return [[NavController], [NavParams]];
+  }
+
+  constructor(nav, navParams) {
+    this.nav = nav;
+
+    // If we navigated to this page, we will have an item available as a nav param
+    this.selectedItem = navParams.get('item');
+
+this.date =  new Date();
+this.AddNotePage = {title:"Add Note",component:AddNotePage};
+this.selectedDate = this.date.getDate()+"/"+ this.date.getMonth() +"/"+ this.date.getFullYear(); 
+
+    this.items = [{"id":"12","court_name":"solapur","first_party_name":"attar","second_party_name":"Akshay"}];
+
+  }
+
+  itemTapped(event, item) {
+     this.nav.push(ItemDetailsPage, {
+       item: item
+     });
+  }
+  
+  
+   openPage(page) {
+       console.log("openpage");
+  }
+  
+}
